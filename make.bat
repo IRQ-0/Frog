73c33ed159ba7fbb5192ce32c69bf912def97d34
IRQ0 <maciej.maloczok@gmail.com>
2023-07-22 02:02:47 +0200

    Added spc.cpp to compilation path

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
 
 del sysmon.exe
-g++ -std=c++11 main.cpp Host.cpp Json.cpp telnet.cpp hostMonitor.cpp static.cpp -lws2_32 -o sysmon.exe
+g++ -std=c++11 main.cpp Host.cpp Json.cpp telnet.cpp hostMonitor.cpp static.cpp spc.cpp RS.cpp -lws2_32 -o sysmon.exe
 sysmon.exe
